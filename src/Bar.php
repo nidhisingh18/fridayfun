aad1e21010fb0f7ee9d41d6b2404104bf15999ed
nidhisingh18 <18nidhisingh@gmail.com>
2018-05-08 11:03:28 +0530

    heloo

--- src/Bar.php
+++ src/Bar.php
@@ -32,7 +32,7 @@ class Bar
      * Get the number.
      * @return float
      */
-    public function getNumberNew()
+    public function getNumberWas()
     {
         return $this->number;
     }
